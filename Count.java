23b2ef559743a6b1662397b317d4c21f3747ce0f
chetan patidar <chetan patidar>
2017-03-23 17:44:32 +0530

    gg

---
+++ Count.java
@@ -0,0 +1,48 @@
+package ses8asi4;
+import java.util.Scanner;
+public class Count {
+
+
+    public static void main(String[] args) throws Exception {
+
+
+        java.io.File file = new java.io.File("E:\\file2.txt");
+
+ 
+
+        Scanner input = new Scanner(file);
+
+        String fileContent = "";
+
+
+        while (input.hasNext()) {
+
+            fileContent += input.next() + " ";
+
+        }
+
+     
+
+        input.close();
+
+        char[] charArr = fileContent.toCharArray();
+
+        int counter = 0;
+
+        for(char c : charArr)
+
+        {
+
+              if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y')
+
+                   counter++;
+
+        }
+
+        
+
+        
+        System.out.println("Number of Vowels: " + counter);
+
+    }
+}
